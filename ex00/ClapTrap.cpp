935feb1444d3755a5414cb1386b24d11d1957b2a
LaLaSero <filia2444@gmsil.com>
2024-11-07 18:08:19 +0900

    ex00

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -20,3 +20,62 @@ ClapTrap::~ClapTrap()
 {
 	std::cout << "ClapTrap destructor called" << std::endl;
 }
+
+ClapTrap &ClapTrap::operator=(const ClapTrap &copy)
+{
+	std::cout << "ClapTrap assignation operator called" << std::endl;
+	_name = copy._name;
+	_hit_points = copy._hit_points;
+	_energy_points = copy._energy_points;
+	_attack_damage = copy._attack_damage;
+	return (*this);
+}
+
+void ClapTrap::attack(std::string const & target)
+{
+	if (_energy_points > 0 && _hit_points > 0)
+	{
+		_energy_points -= 1;
+		std::cout << "ClapTrap " << _name << " attacks " << target << ", causing " << _attack_damage << " points of damage!" << std::endl;
+	}
+	else
+		std::cout << "ClapTrap " << _name << " is out of energy points!" << std::endl;
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+	if (_hit_points > 0)
+	{
+		_hit_points -= amount;
+		std::cout << "ClapTrap " << _name << " takes " << amount << " points of damage!" << std::endl;
+	}
+	else
+		std::cout << "ClapTrap " << _name << " is already dead!" << std::endl;
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	if (_hit_points > 0)
+	{
+		_hit_points += amount;
+		std::cout << "ClapTrap " << _name << " is repaired for " << amount << " hit points!" << std::endl;
+	}
+	else
+		std::cout << "ClapTrap " << _name << " is already dead!" << std::endl;
+}
+
+std::string ClapTrap::getName() const {
+	return _name;
+}
+
+std::string ClapTrap::getHitPoints() const {
+	return std::to_string(_hit_points);
+}
+
+std::string ClapTrap::getEnergyPoints() const {
+	return std::to_string(_energy_points);
+}
+
+std::string ClapTrap::getAttackDamage() const {
+	return std::to_string(_attack_damage);
+}
--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -6,22 +6,26 @@
 
 class ClapTrap
 {
-public:
-	ClapTrap();
-	ClapTrap(std::string name);
-	ClapTrap(const ClapTrap &copy);
-	~ClapTrap();
-	ClapTrap &operator=(const ClapTrap &copy);
+	public:
+		ClapTrap();
+		ClapTrap(std::string name);
+		ClapTrap(const ClapTrap &copy);
+		~ClapTrap();
+		ClapTrap &operator=(const ClapTrap &copy);
 
-	void attack(std::string const & target);
-	void takeDamage(unsigned int amount);
-	void beRepaired(unsigned int amount);
+		void attack(std::string const & target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+		std::string getName() const;
+		std::string getHitPoints() const;
+		std::string getEnergyPoints() const;
+		std::string getAttackDamage() const;
 
-protected:
-	std::string _name;
-	unsigned int _hit_points;
-	unsigned int _energy_points;
-	unsigned int _attack_damage;
+	private:
+		std::string _name;
+		unsigned int _hit_points;
+		unsigned int _energy_points;
+		unsigned int _attack_damage;
 };
 
 #endif
